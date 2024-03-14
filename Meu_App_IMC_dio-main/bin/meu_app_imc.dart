2af9b2998a610ca3502777d5ff6f55ae0bf7f596
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-03-13 23:15:36 -0300

    mark03

--- Meu_App_IMC_dio-main/bin/meu_app_imc.dart
+++ Meu_App_IMC_dio-main/bin/meu_app_imc.dart
@@ -1,15 +1,16 @@
 import 'package:meu_app_imc/models/Pessoa.dart';
+import 'package:meu_app_imc/service/calcular.dart';
 import 'package:meu_app_imc/service/ler_dados.dart';
 import 'package:meu_app_imc/service/tratamento_saidas.dart';
 
 void main(List<String> arguments) {
-  var usuario = Pessoa(
+  final usuario = Pessoa(
       LerConsole.nome(),
       LerConsole.peso(),
       LerConsole.altura()
       );
 
-  var imc = usuario.getPeso() / (usuario.getAltura() * usuario.getAltura());
+  final imc = Calcular.imc(usuario.getPeso(),usuario.getAltura());
   print(
-      'Olá, ${usuario.getNome()} seu IMC é ${imc.toStringAsFixed(2)}, status de ${saidas(imc)}');
+      'Olá, ${usuario.getNome()} seu IMC é $imc, status de ${saidas(imc)}');
 }
--- Meu_App_IMC_dio-main/test/meu_app_imc_test.dart
+++ Meu_App_IMC_dio-main/test/meu_app_imc_test.dart
@@ -1,8 +1,17 @@
-//import 'package:meu_app_imc/meu_app_imc.dart';
-//import 'package:test/test.dart';
 
-//void main() {
-//  test('calculate', () {
-//    expect(calculate(), 42);
-//  });
-//}
+import 'package:meu_app_imc/service/calcular.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+  test('Status de Normal', () {
+    expect(Calcular.imc(60.40,1.60),23.59);
+});
+ test('Status de Sobrepeso', () {
+    expect(Calcular.imc(70,1.60),27.34);
+});
+ test('Status de Obesida de grau II', () {
+    expect(Calcular.imc(90,1.60),35.16);
+});
+
+}
